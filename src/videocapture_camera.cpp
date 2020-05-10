8948e5617cbceec4fb8187fcdffb933f42802662
Karl <a>
2020-05-10 12:28:03 +0200

    write to virtual device, we have video and can update the resolution. Color conversion BGR/RGB and for processed image still to implement.

--- src/videocapture_camera.cpp
+++ src/videocapture_camera.cpp
@@ -2,9 +2,14 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>  // cv::Canny()
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <fcntl.h>
 
 using namespace cv;
 using std::cout; using std::cerr; using std::endl;
@@ -111,11 +116,70 @@ void print_report(const String& winname, const VideoCapture capture)
     }
 }
 
+/// v4l2loopback management
+v4l2_format v4l2_init (int v4l2lo, const VideoCapture cap)
+{ /* Initialize the necessary v4l2 configuration. */
+    struct v4l2_format v;
+    int t;
+    v.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
+    t = ioctl(v4l2lo, VIDIOC_G_FMT, &v);
+    if (t < 0) {
+        cerr << "Unable to read v4l2 format data." << endl;
+        exit(t);
+    }
+
+    v.fmt.pix.width = cap.get(CAP_PROP_FRAME_WIDTH);
+    v.fmt.pix.height = cap.get(CAP_PROP_FRAME_HEIGHT);
+    //Internal format for opencv frames?
+    v.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+    v.fmt.pix.sizeimage = v.fmt.pix.width * v.fmt.pix.height * 3;
+    t = ioctl(v4l2lo, VIDIOC_S_FMT, &v);
+    if (t < 0) {
+        cerr << "Unable to set v4l2 video format." << endl;
+        exit(t);
+    }
+    return v;
+}
+
+int v4l2_refresh_size (int v4l2lo, v4l2_format v, const VideoCapture cap)
+{ /* Match the v4l2 device resolution with the current capture
+     resolution.
+     Note: we need to close and reopen the device for clients such
+           as `ffplay` to be able to pick up the resolution change after
+           they are restarted. No idea why atm. */
+    int t;
+    close(v4l2lo);
+    v4l2lo = open("/dev/video20", O_WRONLY); 
+    if(v4l2lo < 0) {
+        cerr << "Error opening v4l2l device: " << endl;
+        return 1;
+    }
+
+    v.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
+    t = ioctl(v4l2lo, VIDIOC_G_FMT, &v);
+    if (t < 0) {
+        cerr << "Unable to read v4l2 format data." << endl;
+        exit(t);
+    }
+
+    v.fmt.pix.width = cap.get(CAP_PROP_FRAME_WIDTH);
+    v.fmt.pix.height = cap.get(CAP_PROP_FRAME_HEIGHT);
+    v.fmt.pix.sizeimage = v.fmt.pix.width * v.fmt.pix.height * 3;
+    t = ioctl(v4l2lo, VIDIOC_S_FMT, &v);
+    if (t < 0) {
+        cerr << "Unable to refresh v4l2 video format." << endl;
+        exit(t);
+    }
+    return v4l2lo;
+}
+
 int main(int, char**)
 {
     // Create a QT window
     namedWindow("Frame", WINDOW_GUI_NORMAL | WINDOW_NORMAL);
     Mat frame;
+
+    /// Initialize capture.
     cout << "Opening camera..." << endl;
     VideoCapture capture(0); // open the first camera, /dev/video0
     if (!capture.isOpened()) {
@@ -124,6 +188,15 @@ int main(int, char**)
     }
     capture_init(capture);
     print_cap_prop(capture);
+
+    /// Initialize v4l2loopback output.
+    int v4l2lo = open("/dev/video20", O_WRONLY); 
+    if(v4l2lo < 0) {
+        cerr << "Error opening v4l2l device: " << endl;
+        return 1;
+    }
+    v4l2_format v = v4l2_init(v4l2lo, capture);
+
     cout << endl << "Press 'ESC' to quit, 'space' to toggle frame processing"
          << endl;
     cout << endl << "Start grabbing..." << endl;
@@ -138,17 +211,21 @@ int main(int, char**)
         }
         nFrames++;
         print_report("Frame", capture);
-        if (!enableProcessing) {
-            // Display a capture frame without processing.
-            imshow("Frame", frame);
-        }
+        Mat processed;
+        if (!enableProcessing) {processed = frame;}
         else {
-            // Process the captured frame and then display.
+            // Process the captured frame.
             int64 tp0 = cv::getTickCount();
-            Mat processed;
             cv::Canny(frame, processed, 400, 1000, 5);
-            processingTime += cv::getTickCount() - tp0;
-            imshow("Frame", processed);
+        }
+        imshow("Frame", processed);
+        // Write the frame also to the v4l2loopback device:
+	size_t written = write(v4l2lo, processed.data,
+                               processed.total() * processed.elemSize());
+        if (written < 0) {
+            cerr << "Error writing to v4l2loopback device." << endl;
+            close(v4l2lo);
+            return 1;
         }
         // User interaction.
         switch (int key = waitKey(1)) {
@@ -160,7 +237,9 @@ int main(int, char**)
                 cout << "Enable frame processing ('space' key): "
                      << enableProcessing << endl; break;
             case 114/*r*/: //Toggle resolution change.
-                toggle_resolution(capture); break;
+                toggle_resolution(capture); 
+                v4l2lo = v4l2_refresh_size(v4l2lo, v, capture);
+                break;
             case 99/*c*/: //Toggle codec change.
                 toggle_codec(capture); break;
             default: break;
---
+++ virtual-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Create the needed virtual video device at video20.
+
+sudo modprobe -r v4l2loopback
+sudo modprobe v4l2loopback devices=1 video_nr=20 card_label="v4l2loopback" exclusive_caps=1
